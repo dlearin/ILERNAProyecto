cc26f632ff58fe8dc098b3e289bf832aa895ce56
Dlearin <cfernande1408@gmail.com>
2023-06-07 14:03:48 +0200

    implementation of bbdd v2

--- BBDD/escrituraDatos.php
+++ BBDD/escrituraDatos.php
@@ -1,11 +1,11 @@
 <?php
 include 'conexion_db.php';
 
-$device_name = "arduino";
-$led_status = "ON";
-$timestamp = date('Y-m-d H:i:s'); 
+$nombre_dispotivo = "arduino";
+$led_estatus = "ON";
+$tiempoSuceso = date('Y-m-d H:i:s'); 
 
-$sql = "INSERT INTO led_data (device_name, timestamp, led_status) VALUES ('$device_name', '$timestamp', '$led_status')";
+$sql = "INSERT INTO led_data (device_name, timestamp, led_status) VALUES ('$nombre_dispotivo', '$led_estatus', '$tiempoSuceso')";
 
 if ($conn->query($sql) === TRUE) {
   echo "Se ha creado un nuevo registro";
